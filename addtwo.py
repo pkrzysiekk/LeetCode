79e4866782edbd9540176544cf29bf1cfc01e4d6
pkrzysiek <pkrzysiek24@gmail.com>
2026-04-12 19:54:36 +0200

    Delete unused func

--- addtwo.py
+++ addtwo.py
@@ -3,62 +3,43 @@ class ListNode(object):
         self.val = val
         self.next = next
 
-def insertEnd(root,number):
-    new = ListNode(number)
-    if (root==None):
-        return new
-    curr = root
-    while curr.next != None:
-        curr=curr.next 
-    curr.next=new
-    return root
 
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
-        head=ListNode()
-        tail=head
-        carry=0
+        head = ListNode()
+        tail = head
+        carry = 0
 
-        while l1 or l2 or carry !=0:
-            curr1 = l1.val if l1 else 0 
+        while l1 or l2 or carry != 0:
+            curr1 = l1.val if l1 else 0
             curr2 = l2.val if l2 else 0
 
             total = curr1 + curr2 + carry
             carry = total // 10
             digit = total % 10
 
-            tail.next=ListNode(digit)
-            tail=tail.next
+            tail.next = ListNode(digit)
+            tail = tail.next
 
             if l1:
-                l1=l1.next
+                l1 = l1.next
             if l2:
-                l2=l2.next
-            
+                l2 = l2.next
 
         return head.next
 
 
-
-
-
-
-
-        
-
-
 def main():
-    li=ListNode(2)
-    li.next=ListNode(4)
-    li.next.next=ListNode(3)
+    li = ListNode(2)
+    li.next = ListNode(4)
+    li.next.next = ListNode(3)
 
-    li1=ListNode(5)
-    li1.next=ListNode(6)
-    li1.next.next=ListNode(4)
+    li1 = ListNode(5)
+    li1.next = ListNode(6)
+    li1.next.next = ListNode(4)
 
-    sol= Solution()
-    print(sol.addTwoNumbers(li,li1))
+    sol = Solution()
+    print(sol.addTwoNumbers(li, li1))
+
 
 main()
-
-
