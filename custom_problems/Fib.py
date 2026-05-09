f2571b60ff7948c5a6b310ffd563a366ed2f3f61
pkrzysiek <pkrzysiek24@gmail.com>
2026-05-09 14:33:55 +0200

    Create custom folder for problems solved by myself outside of Leetcode

---
+++ custom_problems/Fib.py
@@ -0,0 +1,19 @@
+import sys
+cache = dict()
+
+
+def Fib(n):
+    if n in cache:
+        return cache[n]
+
+    if n == 0:
+        return 0
+    if n == 1 or n == 2:
+        return 1
+    res = Fib(n - 1) + Fib(n - 2)
+    cache[n] = res
+    return res
+
+
+sys.setrecursionlimit(10**9)
+(Fib(1))
