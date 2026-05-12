ea8f829559ae2e0d317e137482bff965283969ef
pkrzysiek <pkrzysiek24@gmail.com>
2026-05-12 16:31:15 +0200

    Add solution for substring with concatenation of words

---
+++ substring_with_concatenation.py
@@ -0,0 +1,40 @@
+class Solution(object):
+    def findSubstring(self, s, words):
+        if not s or not words:
+            return []
+        word_length = len(words[0])
+        total_length = word_length * len(words)
+        result = []
+        word_count = {}
+
+        for word in words:
+            if word in word_count:
+                word_count[word] += 1
+            else:
+                word_count[word] = 1
+
+        for i in range(word_length):
+            left = i
+            sub_count = {}
+            count = 0
+            for j in range(i, len(s) - word_length + 1, word_length):
+
+                sub_word = s[j:j+word_length]
+                if sub_word in word_count:
+                    if sub_word in sub_count:
+                        sub_count[sub_word] += 1
+                    else:
+                        sub_count[sub_word] = 1
+                    count += 1
+                    while sub_count[sub_word] > word_count[sub_word]:
+                        sub_count[s[left:left+word_length]] -= 1
+                        count -= 1
+                        left += word_length
+
+                    if count == len(words):
+                        result.append(left)
+                else:
+                    sub_count.clear()
+                    count = 0
+                    left = j + word_length
+        return result
